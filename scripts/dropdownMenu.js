0298be7c84805179a9073fdcceb2d94d8bd25f47
Tetka95 <martina.bobek95@gmail.com>
2024-11-19 19:57:04 +0100

    obrisani komentari i test skripta

--- scripts/dropdownMenu.js
+++ scripts/dropdownMenu.js
@@ -2,12 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     const menuButton = document.getElementById("menuButton");
     const menu = document.getElementById("menu");
 
-    // Kada se klikne na ikonu menija, dodaj/ukloni klasu 'show'
     menuButton.addEventListener("click", function() {
         menu.classList.toggle("show");
     });
 
-    // Opcionalno: Klik van menija zatvara meni
     document.addEventListener("click", function(event) {
         if (!menuButton.contains(event.target) && !menu.contains(event.target)) {
             menu.classList.remove("show");
--- scripts/script.js
+++
@@ -1,5 +0,0 @@
-var x = 2
-var y = 3
-var zbroj = x + y
-
-console.log(zbroj) //testiranje debug console(obrisati kasnije)
